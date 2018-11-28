48167111c8eda703a2e5f55606b9dee7ec35c811
Shota Shimazu <hornet.live.mf@gmail.com>
2018-11-28 15:24:36 +0900

    revise

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
 func main() {
-	println("Hello, world!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.GET("/message/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "InstaLink is now under the construction.")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
